04d27545a21f66ed282b84f9ebb5dc12ae184989
Elizabeth Silva <sonia.silva@epn.edu.ec>
2024-11-21 08:51:32 -0500

    Main corregido

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,7 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './Pages/Registro/Registro.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './Pages/App.jsx'; // Asegúrate de importar el archivo correcto (App.jsx)
+import { BrowserRouter } from 'react-router-dom'; 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
