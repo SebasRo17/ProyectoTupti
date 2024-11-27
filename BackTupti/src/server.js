74f6bf12dd0a5070c624592f87a2ce65a361d4ab
havicDevs <139989428+bryancito1090@users.noreply.github.com>
2024-11-26 23:40:37 -0500

    Add Login with google service

--- BackTupti/src/server.js
+++ BackTupti/src/server.js
@@ -1,15 +1,29 @@
+require('dotenv').config();
 const express = require('express');
-const swaggerUi = require('swagger-ui-express'); // Importa swagger-ui-express
+const session = require('express-session');
+const passport = require('passport');
+const swaggerUi = require('swagger-ui-express');
 const swaggerConfig = require('./config/swagger');
 const userRoutes = require('./presentation/routes/userRoutes');
+const authRoutes = require('./presentation/routes/authRoutes');
 const { sequelize } = require('./infrastructure/database/mysqlConnection');
+require('./aplication/services/GoogleAuthService'); // Inicializar configuración de passport
 
 const app = express();
 const PORT = 3000;
 
+// Configuración de sesión
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+
+// Configuración de middlewares y rutas
 app.use(express.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerConfig.swaggerSpec)); // Usa swaggerUi.setup
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerConfig.swaggerSpec));
 app.use('/users', userRoutes);
+app.use('/auth', authRoutes);
 
 // Sincronizar con la base de datos y iniciar el servidor
 sequelize.sync()
