b9f3b94064cfcb397f53334c55106488d07b1000
KevColomaDev <kev.enrri@hotmail.com>
2024-12-03 08:27:57 -0500

    fix: dependences and deploy basic structure

--- Frontend/src/Pages/recuperarContrasena/recuperarContrasena.jsx
+++ Frontend/src/Pages/recuperarContrasena/recuperarContrasena.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "./RecuperarContrasena.css";
+import "./recuperarContrasena.css";
 import { HiEye, HiEyeOff } from "react-icons/hi"; // Iconos para mostrar/ocultar contraseña
 
 
