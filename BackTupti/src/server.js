5c26dbeae458be556d165dfb454922ef863ab3c8
KevColomaDev <kev.enrri@hotmail.com>
2024-12-03 08:03:53 -0500

    fix: Server unrunning on port

--- BackTupti/src/server.js
+++ BackTupti/src/server.js
@@ -16,7 +16,6 @@ require('./aplication/services/FacebookAuthService'); // Inicializar configuraci
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // Configuración de CORS
 
